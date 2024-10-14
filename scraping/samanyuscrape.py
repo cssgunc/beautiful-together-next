3582337066e1af9ca419a28aba61bdc81126070e
SamanyuDixit <sdixit48@ad.unc.edu>
2024-10-14 18:10:25 -0400

    Progress on samanyu scraping

--- scraping/samanyuscrape.py
+++ scraping/samanyuscrape.py
@@ -1,10 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://beautifultogethersanctuary.com/available-dogs/"
-response = requests.get(url)
+available_url = "https://beautifultogethersanctuary.com/available-cats/"
+response = requests.get(available_url)
 
-if response.status_code == 200:
-    print("Page fetched successfully!")
-else:
-    print(f"Failed to fetch page. Status code: {response.status_code}")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+heading_elements = soup.find_all('div', class_='col-12 Bzl-dog-heading heading-equal')
+
+cat_names = []
+page_urls = []
+for element in heading_elements:
+    name = element.get_text(strip=True)
+
+    clean_name = re.split(r'\s*Bonded With:|Litter:|\(', name)[0].strip()
+    cat_names.append(clean_name)
+
+
+    a_tag = element.find('a')
+    url = a_tag['href']
+    page_urls.append(url)
+
+
+new_dict = {}
+for i in range(len(cat_names)):
+    new_dict[cat_names[i]] = page_urls[i]
+
+
+breeds = []
+genders = []
+for url in page_urls:
+    animal_page = requests.get(url)
+    animal_soup = BeautifulSoup(animal_page.text, 'html.parser')
+
+    features = animal_soup.find_all('li', class_='features_item')
+
+    for feature in features:
+        breed = feature.get_text(strip=True)
+        breeds.append(breed[0])
+
+    # breed = animal_soup.find('i', class_='icon icon-cat ttip').text.strip()
+    # gender = animal_soup.find_all('i', class_='icon icon-male-sign ttip').text.strip()
+    
+    # breeds.append(breed)
+    # genders.append(gender)
+
+print(breeds)
+
+
+
