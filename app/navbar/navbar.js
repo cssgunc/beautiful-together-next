2bc0c5a298a5fba32c507727c6bab4df2867da95
maia <maiam.unc@gmail.com>
2024-10-13 21:24:43 -0400

    Fixed navbar dropdown alignment and added logo to navbar

--- app/navbar/navbar.js
+++ app/navbar/navbar.js
@@ -16,22 +16,27 @@ const Navbar = (inputs) => {
   //Uses Material UI dependency, RUN npm install
   return (
     <nav className="navbar">
-      <img src="/path/to/your/image.png" alt="Logo" className="logo" />
+      <img src="/logo.png" alt="Logo" className="logo" />
       <div className="navbar-text">{inputs.title}</div>
-      <div className='iconInDiv'>
-      <FavoriteIcon className='heart'/>
-      </div>
-      <div className="dropdown">
-        <div className='iconInDiv'>
-        <DensitySmallIcon onClick={toggleDropdown} className='lines'/>
-        </div>
+      <div>
+        <div className = "icons">
+            <div className='iconInDiv'>
+            <FavoriteIcon className='heart'/>
+            </div>
+            <div className="dropdown">
+              <div className='iconInDiv'>
+              <DensitySmallIcon onClick={toggleDropdown} className='lines'/>
+              </div>
+            </div>
         {dropdownOpen && (
-          <div className="dropdown-content">
-            <a href="/">Home Page</a>
-            <a href="#link2">Link 2</a>
-            <a href="#link3">Link 3</a>
-          </div>
+            <div className="dropdown-content">
+              <a href="/">Home Page</a>
+              <a href="#link2">Link 2</a>
+              <a href="#link3">Link 3</a>
+            </div>
         )}
+        </div>
+          
       </div>
     </nav>
   );
