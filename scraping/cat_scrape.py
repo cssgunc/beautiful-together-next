ae20c6aa9dbe1295926fca58b1ae6a90dd639371
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2024-12-23 04:57:16 -0500

    update .envs

--- scraping/cat_scrape.py
+++ scraping/cat_scrape.py
@@ -7,10 +7,11 @@ import os
 from dotenv import dotenv_values
 
 # Supabase credentials
-config = dotenv_values(".env.local")
-SUPABASE_URL = config['NEXT_PUBLIC_SUPABASE_URL']
-SUPABASE_KEY =  config['NEXT_PUBLIC_SUPABASE_ANON_KEY']
-
+# config = dotenv_values(".env.local")
+# SUPABASE_URL = config['NEXT_PUBLIC_SUPABASE_URL']
+# SUPABASE_KEY =  config['NEXT_PUBLIC_SUPABASE_ANON_KEY']
+SUPABASE_URL = os.getenv('NEXT_PUBLIC_SUPABASE_URL')
+SUPABASE_KEY =  os.getenv('NEXT_PUBLIC_SUPABASE_ANON_KEY')
 
 
 # The table that is edited
