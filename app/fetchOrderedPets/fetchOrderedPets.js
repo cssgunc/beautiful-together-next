f9fd3aaa33c3973240bdd91de9180f9121eb84f4
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2025-04-08 21:52:45 -0400

    Preparing for deployment

--- app/fetchOrderedPets/fetchOrderedPets.js
+++ app/fetchOrderedPets/fetchOrderedPets.js
@@ -12,7 +12,7 @@ export const fetchOrderedPets = async () => {
   
     try {
       if (!preferences) {
-        console.warn('No preferences saved to rank animals. Fetching unranked animals.');
+        //console.warn('No preferences saved to rank animals. Fetching unranked animals.');
         // Fetch unranked animals if no preferences are saved
         const res = await fetch('../api/animals');
         const unrankedAnimals = await res.json();
@@ -29,48 +29,23 @@ export const fetchOrderedPets = async () => {
       console.log(preferences);
   
       if (!response.ok) {
-        console.warn('Failed to fetch ranked animals. Fetching unranked animals instead.');
-        console.error("Response: " + response.status);
+        //console.warn('Failed to fetch ranked animals. Fetching unranked animals instead.');
+        //console.error("Response: " + response.status);
         const res = await fetch('../api/animals');
         const unrankedAnimals = await res.json();
         return unrankedAnimals;
       }
   
       const rankedAnimals = await response.json();
-      console.log('Ranked Animals:', rankedAnimals);
+      //console.log('Ranked Animals:', rankedAnimals);
       return rankedAnimals;
   
     } catch (error) {
-      console.error('An error occurred while fetching ranked animals:', error);
-      console.warn('Fetching unranked animals as fallback.');
+      //console.error('An error occurred while fetching ranked animals:', error);
+      //console.warn('Fetching unranked animals as fallback.');
       const res = await fetch('../api/animals');
 
       const unrankedAnimals = await res.json();
       return unrankedAnimals;
     }
-
-    const response = await fetch("../api/animalsByPreference", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(preferences),
-    });
-    //console.log(preferences);
-
-    if (!response.ok) {
-
-      const res = await fetch("../api/animals");
-      const unrankedAnimals = await res.json();
-      return unrankedAnimals;
-    }
-
-    const rankedAnimals = await response.json();
-    //console.log("Ranked Animals:", rankedAnimals);
-    return rankedAnimals;
-  } catch (error) {
-    const res = await fetch("../api/animals");
-    const unrankedAnimals = await res.json();
-    return unrankedAnimals;
-  }
 };
